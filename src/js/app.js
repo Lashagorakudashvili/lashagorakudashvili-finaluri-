ec3c4e6b86b88ddf1ea8c2dea5e79be4f0471dcf
Lasha gorakudashvili <lashatamo11@gmail.com>
2023-05-10 23:58:57 +0400

    (day 3) (more info finished) (stats (started))

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,20 @@ function changeImage() {
 image.src = images[currentIndex];
 
 setInterval(changeImage, 5000);
+
+
+
+
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const line = document.querySelector('.line-1');
+
+window.addEventListener('scroll', function() {
+  const linePos = line.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (linePos < windowHeight * 0.8) {
+    line.style.width = '100%';
+  }
+});
